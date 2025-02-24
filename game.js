6fbbcd9086069c6ba51025913fe6c5cc9de0297a
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-02-24 12:06:09 +0000

    Written pseudocode and logic for the program. Program is now able to generate a random number between 1 and 3.

---
+++ game.js
@@ -0,0 +1,18 @@
+// Since this is the first JavaScript project being built from scratch, it’s important to remember the wise words from the Problem Solving lesson. For each step in this project, be sure to do the following
+
+// Plan or pseudocode your solution:
+// SET a random number to COMPUTE the user's INPUT.
+// Ask user for an INPUT (paper, rock or scissors).
+// Check the INPUT and COMPUTE it for an OUTPUT.
+// PRINT the OUTPUT with the results.
+
+// Write the code.
+let machineChoice = Math.floor(Math.random() * 3) + 1;
+
+function getComputerChoice(machine){
+
+}
+
+console.log(machineChoice);
+
+// Test your code to make sure it works.
