802314061b4008fce03cf523952a476d1a79571c
Cremoide <62185350+Cremoide@users.noreply.github.com>
2025-03-27 14:00:06 +0000

    Reworked logic on single round, created an ugly UI (for the moment), moved announcements from console to the actual website (scores counter, round winner text, etc). Program still need some tweaks to play 5 games, but everything is working for now.

--- game.js
+++ game.js
@@ -8,118 +8,132 @@
 
 // Write the code.
 console.log("Hi Dev! Welcome to the game.");
-alert(
-  "You're now playing rock papper scissors! Play 5 rounds to beat the machine!"
-);
-// console.log("You're now playing rock papper scissors!");
+
+// Declare the round counter variable
+var roundsPlayed = 0;
 
 // Declare the players score variables
 var computerScore = 0;
 var humanScore = 0;
 
-var roundsPlayed = 0;
+// Logic to ge via button for each round
+let buttons = document.querySelector("#buttons");
 
-// Write the logic to play the entire game
-function playGame() {
-  roundsPlayed++;
-  console.log("Round:", roundsPlayed);
+buttons.addEventListener("click", function (e) {
+  let humanSelection = e.target.id;
 
-  // Logic to get the computer choice
-  function getComputerChoice() {
-    let a = Math.floor(Math.random() * 3);
-    // console.log("Machine says:", a);
-    if (a < 1) {
-      a = "rock";
-    } else if (a == 1) {
-      a = "paper";
-    } else if (a > 1) {
-      a = "scissors";
-    }
-    console.log("Machine says:", a);
-    return a;
+  switch (humanSelection) {
+    case "rock":
+      console.log("You chose: " + humanSelection);
+      playRound(humanSelection, getComputerChoice());
+      break;
+    case "paper":
+      console.log("You chose: " + humanSelection);
+      playRound(humanSelection, getComputerChoice());
+      break;
+    case "scissors":
+      console.log("You chose: " + humanSelection);
+      playRound(humanSelection, getComputerChoice());
+      break;
   }
 
-  // Logic to get the human choice
-  function getHumanChoice() {
-    let b = prompt(
-      "Choose between rock, paper or scissors. (Write your bet).",
-      "rock"
-    ).toLowerCase();
-    if (b == "rock") {
-      b = "rock";
-    } else if (b == "paper") {
-      b = "paper";
-    } else if (b == "scissors") {
-      b = "scissors";
-    } else {
-      alert("Choose a valid item!");
-      getHumanChoice();
-    }
-    console.log("You say:", b);
-    // console.log("So you saying:", b);
-    return b;
-  }
+  // Updates score after playRound
+  const scoreDiv = document.querySelector("#scoreDiv");
 
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
+  document.getElementById(
+    "score"
+  ).textContent = `Machine:  ${computerScore} Human: ${humanScore}`;
 
-  // Write the logic to play a single round
-  function playRound(humanSelection, computerSelection) {
-    if ((humanSelection == "rock") && (computerSelection == "rock")) {
-      alert("It's a tie! Try again...");
-      return;
-    } else if ((humanSelection == "paper") && (computerSelection == "paper")) {
-      alert("It's a tie! Try again...");
-      return;
-    } else if (
-      (humanSelection == "scissors") &&
-      (computerSelection == "scissors")
-    ) {
-      alert("It's a tie! Try again...");
-      return;
-    } else if ((humanSelection == "rock") && (computerSelection == "paper")) {
-      alert("You loose this round!");
-      return computerScore++;
-    } else if (
-      (humanSelection == "paper") &&
-      (computerSelection == "scissors")
-    ) {
-      alert("You loose this round!");
-      return computerScore++;
-    } else if ((humanSelection == "scissors") && (computerSelection == "rock")) {
-      alert("You loose this round!");
-      return computerScore++;
-    } else if ((computerSelection == "rock") && (humanSelection == "paper")) {
-      alert("You win this round!");
-      return humanScore++;
-    } else if (
-      (computerSelection == "paper") &&
-      (humanSelection == "scissors")
-    ) {
-      alert("You win this round!");
-      return humanScore++;
-    } else if ((computerSelection == "scissors") && (humanSelection == "rock")) {
-      alert("You win this round!");
-      return humanScore++;
-    }
-  }
-  playRound(humanSelection, computerSelection);
+  scoreDiv.appendChild(score);
+
+  ////////////////////////////////////////////////////////////
   console.log("Machine:", computerScore, "Human:", humanScore);
+});
 
-  if ((roundsPlayed == 5)) {
-    if (computerScore > humanScore) {
-      console.log("Machine wins.");
-      console.log("FINAL SCORE: Machine:", computerScore, "Human:", humanScore);
-      alert("Machine wins! Press F5 to play again...");
-    } else if (humanScore > computerScore) {
-      console.log("Human win.");
-      console.log("FINAL SCORE: Machine:", computerScore, "Human:", humanScore);
-      alert("You win! Press F5 to play again...");
-    }
-  } else {
-    playGame();
+// Logic to get the computer choice (WORKING)
+function getComputerChoice() {
+  let rps = ["rock", "paper", "scissors"];
+  let a = rps[Math.floor(Math.random() * rps.length)];
+  console.log("Machine says:", a);
+  return a;
+}
+
+// Write the logic to play the entire game (NOT IN USE YET)
+// function playGame() {
+//   roundsPlayed++;
+//   console.log("Round:", roundsPlayed);
+// }
+
+function displayResults() {
+  const resultsDiv = document.querySelector("#resultsDiv");
+
+  document.getElementById("result").textContent = "It's a tie! Try again...";
+
+  resultsDiv.appendChild(result);
+}
+
+// Logic to play a single round
+function playRound(humanSelection, computerSelection) {
+  if (humanSelection == "rock" && computerSelection == "rock") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "It's a tie! Try again...";
+    resultsDiv.appendChild(result);
+    return;
+  } else if (humanSelection == "paper" && computerSelection == "paper") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "It's a tie! Try again...";
+    resultsDiv.appendChild(result);
+    return;
+  } else if (humanSelection == "scissors" && computerSelection == "scissors") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "It's a tie! Try again...";
+    resultsDiv.appendChild(result);
+    return;
+  } else if (humanSelection == "rock" && computerSelection == "paper") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "You loose this round!";
+    resultsDiv.appendChild(result);
+    return computerScore++;
+  } else if (humanSelection == "paper" && computerSelection == "scissors") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "You loose this round!";
+    resultsDiv.appendChild(result);
+    return computerScore++;
+  } else if (humanSelection == "scissors" && computerSelection == "rock") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "You loose this round!";
+    resultsDiv.appendChild(result);
+    return computerScore++;
+  } else if (computerSelection == "rock" && humanSelection == "paper") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "You win this round!";
+    resultsDiv.appendChild(result);
+    return humanScore++;
+  } else if (computerSelection == "paper" && humanSelection == "scissors") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "You win this round!";
+    resultsDiv.appendChild(result);
+    return humanScore++;
+  } else if (computerSelection == "scissors" && humanSelection == "rock") {
+    const resultsDiv = document.querySelector("#resultsDiv");
+    document.getElementById("result").textContent = "You win this round!";
+    resultsDiv.appendChild(result);
+    return humanScore++;
   }
 }
 
+// if ((roundsPlayed == 5)) {
+//   if (computerScore > humanScore) {
+//     console.log("Machine wins.");
+//     console.log("FINAL SCORE: Machine:", computerScore, "Human:", humanScore);
+//     alert("Machine wins! Press F5 to play again...");
+//   } else if (humanScore > computerScore) {
+//     console.log("Human win.");
+//     console.log("FINAL SCORE: Machine:", computerScore, "Human:", humanScore);
+//     alert("You win! Press F5 to play again...");
+//   }
+// } else {
+//   playGame();
+// }
+
 // Test your code to make sure it works.
-playGame();
